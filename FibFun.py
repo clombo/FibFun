1c2e6b24464aa9c6288fd79d68fb51c6743f5bc9
clombo <christo.lombard@live.co.za>
2018-01-20 02:48:52 -0800

    Final changes made

--- FibFun.py
+++ FibFun.py
@@ -10,15 +10,14 @@ class FibFun(object):
     
     def GetList(self):
         return self._FibList
-        print self._FibList
 
     def Fibnth(self,n):
         for i in range(n):
             self._a,self._b = self._b,self._a+self._b
             self._FibList += ",{}".format(self._b)
         
-        self._last = self._a
-        return self._a
+        self._last = self._b
+        return self._b
 
 
 
@@ -27,23 +26,22 @@ def Main():
 
     parser.add_argument("num",help="Integer value: The fibonacci number you wish to calculate",type=int)
     parser.add_argument("-fl","--fiblist",help="Include full list of fibonacci numbers calculated in response",action="store_true")
-    parser.add_argument("-v","--verbose",help="Increase output verbosity",action="store_true")
+    parser.add_argument("-v","--verbose",help="Increase output verbosity. When specifying this argument you do not need to include --fiblist argument to get list",action="store_true")
 
     args = parser.parse_args()
 
-    tAllVerbose = Template("The nth fibonacci number is : $Fib. \n Full list of numbers generated: $List")
+    tAllVerbose = Template("The nth fibonacci number is : $Fib. \nFull list of numbers generated: $List")
 
     fib = FibFun()
 
-    fData = dict(Fib=fib.Fibnth(args.num),List=fib.GetList)
-    print fib.GetList
+    fData = dict(Fib=fib.Fibnth(args.num),List=fib.GetList())
 
-  #  if args.verbose:
-  #      print tAllVerbose.substitute(fData)
-  #  elif args.fiblist:
-  #      print fData.get("List")
-  #  else:
-  #      print fData["Fib"]
+    if args.verbose:
+        print tAllVerbose.substitute(fData)
+    elif args.fiblist:
+        print fData.get("List")
+    else:
+        print fData["Fib"]
 
 if __name__ == "__main__":
     Main()
---
+++ README.md
@@ -0,0 +1,21 @@
+#Summary
+
+Just a test script to test out the Template and argparse python modules. Also build a object to generate the fibonacci numbers
+which is returned in the script when you run it via command line.\
+
+#Usage
+
+Number value is required to run the script.
+
+##Example
+
+```
+python FibFun.py 6
+```
+
+This will generate the 6th fibonacci number and print it to the screen.
+For more commands you can type:
+
+```
+python FibFun.py -h
+```
